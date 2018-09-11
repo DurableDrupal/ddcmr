4309a98f6c89c78e9a1d4e91195a7de535caa60a
victorkane <victorkane@gmail.com>
2018-09-11 08:31:43 -0400

    for #1 using Text schema instead of String for summaries and descriptions

--- scs/server/models/blog.md
+++ scs/server/models/blog.md
@@ -1,13 +1,14 @@
 var mongoose = require('./db')
 var MetaData = require('./base-content-attributes/meta-data').MetaData
 var Tag = require('./base-content-attributes/tag').Tag
+var Text = require('./base-content-attributes/text').Text
 
 var blogSchema = mongoose.Schema({
   idLegacy: Number,
   metaData: MetaData.schema,
   author: [{type: mongoose.Schema.Types.ObjectId, ref: 'Author'}],
-  blogSummary: String,
-  blogDescription: String,
+  blogSummary: Text.schema,
+  blogDescription: Text.schema,
   tags: [Tag.schema]
 })
 
--- scs/server/models/case-study.js
+++ scs/server/models/case-study.js
@@ -1,13 +1,14 @@
 var mongoose = require('./db')
 var MetaData = require('./base-content-attributes/meta-data').MetaData
 var Tag = require('./base-content-attributes/tag').Tag
+var Text = require('./base-content-attributes/text').Text
 
 var caseStudySchema = mongoose.Schema({
   idLegacy: Number,
   metaData: MetaData.schema,
   articles: [{ weight: Number, article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article'} }],
-  caseStudySummary: String,
-  caseStudyDescription: String,
+  caseStudySummary: Text.schema,
+  caseStudyDescription: Text.schema,
   tags: [Tag.schema]
 })
 
