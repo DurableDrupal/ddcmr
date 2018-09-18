e845f165ac7ab956b306c9eddcab6ea64af25745
victorkane <victorkane@gmail.com>
2018-09-18 14:38:53 -0400

    For #2 store server side hydration for top level case studies section, top level page section hydration of associated article and list of case studies, rendering of associated article

--- cwa/pages/case-studies/index.vue
+++ cwa/pages/case-studies/index.vue
@@ -6,25 +6,21 @@
     <v-container fluid grid-list-md fill-height>
       <v-layout row wrap>
 
-<!-- start first content item with heading -->
-        <v-flex xs12>
+<!-- start first content row -->
+        <v-flex xs12 sm6>
+          <ArticleFull
+            :article="selectLoadedArticle('case-studies-intro')"
+            back=false
+          ></ArticleFull>
+        </v-flex>
+        <v-flex xs12 sm6>
           <v-card>
             <v-card-title class="primary--text headline">Case Studies</v-card-title>
             <div>
-              <p class="pa-2">
-              So aren't you sick and tired of tutorials that show how "awesome" and "easy" full stack javascript is, and how you can whip up an app in 5 minutes? Sometimes that's even true. But the apps used in all the shallow examples 99% of the time have one thing in common: they are completely trivial and never touch the real world on several important points:
-              </p>
-
-              <ul><li>They are rarely re-usable</li>
-                  <li>They almost always include some kind of "free" but actually paid service on the backend</li>
-                 <li>They don't take into account how to do things like update to newer versions of frameworks, or deploy apps</li>
-                 <li>They often do nothing more than repeat what is already in the docs</li></ul>
-
-               <p>Well, we aren't going to show any over-simplified examples here, everything is a full-blown case study that goes through the complete cycle of disciplines your team really needs to master in order to actually launch real world apps.</p>
             </div>
           </v-card>
         </v-flex>
-<!-- end first content item with heading -->
+<!-- end first content row -->
 
       </v-layout>
     </v-container>
@@ -34,3 +30,25 @@
 <!-- end page layout -->
 </template>
 
+<script>
+import ArticleFull from '@/components/articles/full'
+
+export default {
+  components: {
+    ArticleFull
+  },
+  computed: {
+    loadedArticles() {
+      const articles = this.$store.state.loadedPageArticlesCaseStudies
+      return {
+        articles
+      }
+    }
+  },
+  methods: {
+    selectLoadedArticle(slug) {
+      return this.loadedArticles.articles.find(a => a.metaData.itemSlug === slug )
+    }
+  }
+}
+</script>
--- cwa/store/index.js
+++ cwa/store/index.js
@@ -3,17 +3,29 @@ import Vuex from "vuex"
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      loadedPageArticlesWhatWhy: []
+      loadedPageArticlesWhatWhy: [],
+      loadedPageArticlesCaseStudies: [],
+      loadedCaseStudies: [],
     },
     mutations: {
       setPageArticlesWhatWhy(state, articles) {
         state.loadedPageArticlesWhatWhy = articles
       },
+      setPageArticlesCaseStudies(state, articles) {
+        state.loadedPageArticlesCaseStudies = articles
+      },
+      setCaseStudies(state, articles) {
+        state.loadedCaseStudies = articles
+      }
     },
     actions: {
       async nuxtServerInit(vuexContext, context) {
         const theLoadedPageArticlesWhatWhy = await context.app.$axios.$get (context.env.API_HOST + "/api/articles/tag/what-why")
         vuexContext.commit("setPageArticlesWhatWhy", theLoadedPageArticlesWhatWhy)
+        const theLoadedPageArticlesCaseStudies = await context.app.$axios.$get (context.env.API_HOST + "/api/articles/tag/case-studies")
+        vuexContext.commit("setPageArticlesCaseStudies", theLoadedPageArticlesCaseStudies)
+        const theLoadedCaseStudies = await context.app.$axios.$get (context.env.API_HOST + "/api/case-studies?select=-articles")
+        vuexContext.commit("setCaseStudies", theLoadedCaseStudies)
       },
     }
   })
