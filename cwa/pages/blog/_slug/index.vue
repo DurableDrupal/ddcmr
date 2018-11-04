0cb5fc4b2f7e541dacce22fdd376eb808cde5432
victorkane <victorkane@gmail.com>
2018-11-04 09:46:24 -0500

    For #2  blog section appropriate header and body font and styling plus 45-75 characters per body line for blog post detail full page

--- cwa/pages/blog/_slug/index.vue
+++ cwa/pages/blog/_slug/index.vue
@@ -7,10 +7,18 @@
       <v-layout row wrap>
 
 <!-- start content item -->
-        <v-flex xs12>
+        <v-flex xs12 offset-md2 md5 sm7>
           <ArticleFull
             :article="article"
           ></ArticleFull>
+        </v-flex>
+        <v-flex xs12 sm4 offset-sm1>
+          <v-card class="pa-2" flat>
+        <AuthorFull
+          :author="article.author[0]"
+          :back=false
+        ></AuthorFull>
+          </v-card>
         </v-flex>
 <!-- end content item -->
 
@@ -24,10 +32,12 @@
 
 <script>
 import ArticleFull from '@/components/articles/full'
+import AuthorFull from '@/components/authors/full'
 
 export default {
   components: {
-    ArticleFull
+    ArticleFull,
+    AuthorFull
   },
   async asyncData(context) {
     const article = await context.app.$axios.$get(context.env.API_HOST + '/api/articles/slug/' + context.route.params.slug)
