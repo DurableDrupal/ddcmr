2d7ae1773a497dccacba639101bbda6debf14b8c
victorkane <victorkane@gmail.com>
2018-11-11 06:11:06 -0500

    For #2 vastly improve steps you can take layout

--- cwa/pages/steps-you-can-take/index.vue
+++ cwa/pages/steps-you-can-take/index.vue
@@ -2,20 +2,61 @@
 <!-- start page layout -->
   <v-layout column>
 
-<!-- start first content item with heading -->
+<!-- start grid layout -->
     <v-container fluid grid-list-md fill-height>
       <v-layout row wrap>
+
+<!-- start first content item with heading -->
         <v-flex xs12>
           <v-card>
             <v-card-title class="primary--text headline">Steps you can take</v-card-title>
           </v-card>
         </v-flex>
-      </v-layout>
-    </v-container>
-    <v-container fluid grid-list-md fill-height>
-      <v-layout row wrap>
-  <v-flex xs12>
-  <v-expansion-panel popout>
+        <v-flex xs12 sm4>
+          <v-card>
+            <v-card-text class="pb-0">
+              <div>
+              <p class="subheading">
+                Grab the code on GitHub (please star us and help us grow the community!)
+              </p>
+              </div>
+            </v-card-text> 
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm4>
+          <v-card>
+            <v-card-text class="pb-0">
+              <div>
+              <p class="subheading">
+                Read the books, check out the case studies, follow the guides below
+              </p>
+              </div>
+            </v-card-text> 
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm4>
+          <v-card>
+            <v-card-text class="pb-0">
+              <div>
+              <p class="subheading">
+                Use our mentoring services and finish your high priority projects fast!
+              </p>
+              </div>
+            </v-card-text> 
+          </v-card>
+        </v-flex>
+<!-- end first content item with heading -->
+
+        <v-flex xs12>
+          <v-card>
+            <v-card-title class="primary--text headline">Guides you can follow step-by-step</v-card-title>
+            <v-card-text>
+              <p>Handy alphabetical index to categories (click to open) makes it easy to find individual guides that forming part of the Case Studies.</p>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+ <v-expansion-panel popout>
     <v-expansion-panel-content
       v-for="(stepFilter,i) in loadedStepFilters.stepFilters"
       :key="i"
@@ -29,9 +70,8 @@
                 <v-divider></v-divider>
               </div>
     </v-expansion-panel-content>
-  </v-expansion-panel>
+</v-expansion-panel>
         </v-flex>
-<!-- end first content item with heading -->
 
       </v-layout>
     </v-container>
