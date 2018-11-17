d2d17ab54f07fe64426fd21ffa112c937042f94f
victorkane <victorkane@gmail.com>
2018-11-17 20:45:52 +0000

    For #4 scaffold cms utility to cover all logical alternatives (all content items, all content items for a single content type, a single content item, plus taking into account whether or not the file belongs to a content sub type

--- cms/index.js
+++ cms/index.js
@@ -20,21 +20,41 @@ inquirer.prompt(questions).then(answers => {
   var dirname = path.basename(path.dirname(answers.file))
   var basename = path.basename(answers.file)
   var extname = path.extname(answers.file)
-  console.log('basename', basename)
-  console.log('extname', extname)
+  // console.log('basename', basename)
+  // console.log('extname', extname)
+  var patharray = answers.file.split('/')
+  // where in the path array is the dir "content"?
+  var contentindex = patharray.indexOf('content')
+  // by default, there will not be any grandfather content type
+  var dirname2 = ''
   fs.stat(answers.file, function(err, stats) {
     if (stats.isDirectory()) {
-      console.log('directory')
+      // console.log('directory')
       if (basename === 'content') {
         console.log('upsert all')
       } else {
-        console.log('upsert all', basename)
+        // console.log(patharray)
+        // console.log('length', patharray.length, 'contentindex', contentindex)
+        if (patharray.length - contentindex > 2) {
+          dirname2 = patharray[contentindex + 1]
+          console.log('upsert all for content type', dirname2, 'and content sub type', basename)
+        } else {
+          console.log('upsert all', basename)
+        }
       }
     }
     if (stats.isFile()) {
-      console.log('file')
-      console.log('upsert content item ', basename, ' for content type ', dirname)
+      // console.log(patharray)
+      // console.log('length', patharray.length, 'contentindex', contentindex)
+      // if this is a file in a subdir (sub content type) underneath the content type identify it so we can provide it as parameter to upsert script
+      if (patharray.length - contentindex > 3) {
+        dirname2 = patharray[contentindex + 1]
+        console.log('upsert content item', basename, 'for content type', dirname2, 'and content sub type', dirname)
+      } else {
+        // console.log('file')
+        console.log('upsert content item', basename, 'for content type', dirname)
+      }
     }
-    console.log('stats', stats)
+    // console.log('stats', stats)
   })
 })
