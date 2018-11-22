5e7febfbae3d20f9bfb917f38d1d9ba42e694b05
victorkane <victorkane@gmail.com>
2018-11-22 20:05:35 +0000

    For #4 use user friendly fuzzy search for directories only for restore, automatic datetimestamp creation of backup names, restore to local environment db only, from any env backup

--- utils/index.js
+++ utils/index.js
@@ -2,45 +2,49 @@
 var inquirer = require('inquirer')
 var shell = require('shelljs')
 var strftime = require('strftime')
+var fs = require('fs')
+var path = require('path')
 require('dotenv').config()
 
 var thetimestamp = strftime('%Y%m%d%H%M%S', new Date())
+
+inquirer.registerPrompt('fuzzypath', require('inquirer-fuzzy-path'))
 
 var questions = [
   {
     type: 'list',
     name: 'op',
     message: 'Would you like to backup, restore or zap the scs db?',
-    choices: ['backup', 'restore', 'zap', 'cancel']
+    choices: ['backup', 'restore to this env', 'zap', 'cancel']
   },
   {
-    type: 'input',
+    type: 'fuzzypath',
     name: 'bname',
-    message: 'Timestamp as name for backup/restore?',
-    default: thetimestamp,
+    rootPath: process.env.BACKUP_DIR,
+    pathFilter: (isDirectory, nodePath) => isDirectory,
+    message: 'Choose BSON data dump folder you wish to restore (./<timestamp>/ddcmr_dev or ./<timestamp>/ddcmr_prod)',
     when: function(answers) {
-      return (answers.op === 'backup' || answers.op === 'restore')
+      return (answers.op === 'restore to this env')
     }
   }
 ]
 
 inquirer.prompt(questions).then(answers => {
-  // console.log(JSON.stringify(answers, null, '  '));
-  // console.log('\nYou said: ', answers.op + "\n" );
   switch(answers.op) {
     case 'backup':
-      // console.log('\nYou said: backup' + "\n" );
-      // console.log('answers', answers)
-      var thecmd = 'mongodump -d ' + process.env.DB_DB + ' -o ' + process.env.BACKUP_DIR + '/'  + answers.bname + ' -u ' + process.env.DB_USER + ' -p ' + process.env.DB_PASSWORD + ' --authenticationDatabase admin'
-      // console.log('thecmd', thecmd + "\n")
+      // we always use an automatically generated timestamp for backup directory name
+      console.log("\nBacking up to ", process.env.BACKUP_DIR + '/' + thetimestamp + "\n")
+      var thecmd = 'mongodump -d ' + process.env.DB_DB + ' -o ' + process.env.BACKUP_DIR + '/'  + thetimestamp + ' -u ' + process.env.DB_USER + ' -p ' + process.env.DB_PASSWORD + ' --authenticationDatabase admin'
       if (shell.exec(thecmd).code !== 0) {
         shell.echo('Error: Backup failed');
         shell.exit(1);
       }
       break
-    case 'restore':
-      var thecmd = 'mongorestore --drop --db ' + process.env.DB_DB + ' -u ' + process.env.DB_USER + ' -p ' + process.env.DB_PASSWORD + ' --authenticationDatabase admin ' + process.env.BACKUP_DIR + '/' + answers.bname + '/' + process.env.DB_DB 
-      // console.log('thecmd', thecmd + "\n")
+    case 'restore to this env':
+      // we always use fuzzy search for backup directory name bname
+      // extract db name
+      var dbname = process.env.DB_DB
+      var thecmd = 'mongorestore --drop --db ' + dbname + ' -u ' + process.env.DB_USER + ' -p ' + process.env.DB_PASSWORD + ' --authenticationDatabase admin ' + answers.bname
       if (shell.exec(thecmd).code !== 0) {
         shell.echo('Error: Restore failed');
         shell.exit(1);
@@ -48,7 +52,6 @@ inquirer.prompt(questions).then(answers => {
       break
     case 'zap':
       var thecmd = 'mongo ' + process.env.DB_DB + ' -u ' + process.env.DB_USER + ' -p ' + process.env.DB_PASSWORD + ' --authenticationDatabase admin --eval "db.dropDatabase();"'
-      // console.log('thecmd', thecmd + "\n")
       if (shell.exec(thecmd).code !== 0) {
         shell.echo('Error: Zap failed');
         shell.exit(1);
