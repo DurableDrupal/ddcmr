93cad7cddc4247ac56e0e303fc9de45a46353398
victorkane <victorkane@gmail.com>
2018-12-05 13:39:41 +0000

    For #2 load books in store ordered by createdData metaData

--- cwa/store/index.js
+++ cwa/store/index.js
@@ -48,7 +48,7 @@ const createStore = () => {
         vuexContext.commit("setCaseStudies", theLoadedCaseStudies)
         const theLoadedPageArticlesBooks = await context.app.$axios.$get (context.env.API_HOST + "/api/articles/tag/books")
         vuexContext.commit("setPageArticlesBooks", theLoadedPageArticlesBooks)
-        const theLoadedBooks = await context.app.$axios.$get (context.env.API_HOST + "/api/books?select=-articles")
+        const theLoadedBooks = await context.app.$axios.$get (context.env.API_HOST + "/api/books?select=-articles&sort=metaData.createdDate")
         vuexContext.commit("setBooks", theLoadedBooks)
         // sort by published date desc
         const theLoadedPageArticlesBlog = await context.app.$axios.$get (context.env.API_HOST + "/api/articles/tag/blog?sort=-metaData.publishedDate")
