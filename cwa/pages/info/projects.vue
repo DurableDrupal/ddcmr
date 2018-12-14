966697c23cb155c0b8b2e6a84b561eb26c4c80d1
victorkane <victorkane@gmail.com>
2018-12-14 03:36:33 +0000

    For #2 include placeholder paragraph for mentoring and projects google form landing pages

--- cwa/pages/info/projects.vue
+++ cwa/pages/info/projects.vue
@@ -2,6 +2,7 @@
   <v-layout>
     <v-flex xs12 class="info-page">
       <div class="g-form">
+        <p>placeholder for google form Tell me about your project</p>
         <!--
         <iframe src="#google-or-marketing-form" width="640" height="1124" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
         -->
